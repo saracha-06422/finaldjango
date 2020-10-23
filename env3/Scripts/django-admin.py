c22e7a5602c658c77d6d2da9b04539f58bcd3cc1
saracha <saracha06422@gmail.com>
2020-10-23 11:17:03 +0700

    kclone master

---
+++ env3/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\saracha\desktop\4th\inventory\inventory\env3\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
